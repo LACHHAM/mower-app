877fa615444fc9be13b84436b1e2914ab85f1011
LACHHAM <hamza.lachkham@outlook.com>
2024-07-22 15:17:59 +0200

    init project commit

---
+++ src/main/java/org/hla/mowitnow/mower_app/MowerAppApplication.java
@@ -0,0 +1,13 @@
+package org.hla.mowitnow.mower_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MowerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MowerAppApplication.class, args);
+	}
+
+}
