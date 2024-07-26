555b3d8e4b4857d8ddf58221b3dbb5ea1305e12e
LACHHAM <hamza.lachkham@outlook.com>
2024-07-26 10:15:21 +0200

    refacto code

--- src/test/java/org/hla/mowitnow/mower_app/services/DirectionServiceTest.java
+++ src/test/java/org/hla/mowitnow/mower_app/services/DirectionServiceTest.java
@@ -1,12 +1,7 @@
 package org.hla.mowitnow.mower_app.services;
 
 import org.hla.mowitnow.mower_app.models.Direction;
-import org.hla.mowitnow.mower_app.models.Orientation;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
