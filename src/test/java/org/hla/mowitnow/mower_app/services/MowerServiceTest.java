0488487abbf1abe161243a0c8e97c2a7cd678616
LACHHAM <hamza.lachkham@outlook.com>
2024-07-26 10:15:21 +0200

    refacto code

--- src/test/java/org/hla/mowitnow/mower_app/services/MowerServiceTest.java
+++ src/test/java/org/hla/mowitnow/mower_app/services/MowerServiceTest.java
@@ -3,20 +3,12 @@ package org.hla.mowitnow.mower_app.services;
 import org.hla.mowitnow.mower_app.models.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class MowerServiceTest {
--- src/test/java/org/hla/mowitnow/mower_app/services/OrientationServiceTest.java
+++ src/test/java/org/hla/mowitnow/mower_app/services/OrientationServiceTest.java
@@ -3,13 +3,9 @@ package org.hla.mowitnow.mower_app.services;
 
 import org.hla.mowitnow.mower_app.models.Orientation;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
